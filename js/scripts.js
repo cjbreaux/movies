f7441483a41667e5355993c1a6af92bc7fdced1a
Chris Breaux and Ward Chamberlain <ward@>
2019-01-29 16:33:17 -0800

    add UI logic to project

--- js/scripts.js
+++ js/scripts.js
@@ -41,14 +41,62 @@ function Movies(title,showtimes,basePrice,rating) {
   this.rating = rating;
 }
 
-Movies.prototype.getTicketPrice(basePrice, age, showtime) {
-  var ticketPrice = 0;
+Movies.prototype.getTicketPrice = function(basePrice, age, showtime) {
+  var ticketPrice = basePrice;
   if(showtime < 1500) {
     ticketPrice = 6;
-  } else if (age > 65 || age < 10) {
-    ticketPrice = basePrice * .8;
+  } else if (age === 'Senior' || age === 'Child') {
+    ticketPrice = ticketPrice * .8;
   }
   return ticketPrice;
 }
 
-var newMovie = new Movie()
+Movies.prototype.displayTimes = function() {
+  var htmlOutput = "";
+  this.showtimes.forEach(function(showtime){
+    htmlOutput += "<option value='" + showtime + "'>" + showtime + "</option>";
+  });
+  $(".timeSelector").html(htmlOutput);
+}
+
+//function timeCovert(time){ convert time to pleasing format later
+// }
+
+//var newMovie = new Movie()
+Movies.prototype.showTicketInfo = function(showtime, age){
+  $(".title").html(this.title)
+  $(".showtime").html(showtime)
+  $(".rating").html(this.rating)
+  $(".ticketPrice").html(this.getTicketPrice(this.basePrice, age, showtime));
+
+}
+
+//ui logic
+var myTheater = new Theater();
+var spider = new Movies("Spiderman", [1000,1200,1500,1900], 8, "PG-13")
+var rhap = new Movies("Bohemian Rhapsody", [1200,1500,2100], 8, "R")
+var dune = new Movies("Dune",[1200,2300],8, "R")
+var dog = new Movies("A Dogs Way Home", [1000,1300,1800], 8, "PG")
+myTheater.addMovie(spider);
+myTheater.addMovie(rhap);
+myTheater.addMovie(dune);
+myTheater.addMovie(dog);
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+    event.preventDefault();
+    var inputMovie = $(".movieSelector option:selected").val();
+    var inputAge = $(".ageSelector option:selected").val();
+    var inputTime = $(".timeSelector option:selected").val();
+    var selectedMovie = myTheater.findMovie(inputMovie);
+    selectedMovie.showTicketInfo(inputTime, inputAge);
+  });
+
+  $(".movieSelector").change(function(){
+    var movieName = $(".movieSelector option:selected").val();
+    var movie = myTheater.findMovie(movieName);
+    movie.displayTimes();
+  });
+
+
+})
